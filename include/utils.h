fef427a9213c0606951b45074c938920c60c1c34
bobo <mucha.bobo@gmail.com>
2010-10-02 00:24:24 +0200

    Added documentation for utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,13 +1,20 @@
 /* 
  * File:   utils.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 20. červenec 2010, 23:21
+ * 
+ * Contains some utility functions.
  */
 
 #ifndef UTILS_H
 #define	UTILS_H
 
+/*
+ * Following functions translate numbers into strings. They return staticly 
+ * allocated string, so result is valid only until next call, butt it is not 
+ * needed to free him. 
+ */
 const char* int_to_string (long int value);
 
 const char* uint_to_string (long unsigned int value);
