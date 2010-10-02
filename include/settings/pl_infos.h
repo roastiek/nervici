3658ad5631bb35dcc9499dba125aaf1718cd13e7
bobo <mucha.bobo@gmail.com>
2010-10-02 17:39:56 +0200

    Added documentation of pl_infos.h

--- include/settings/pl_infos.h
+++ include/settings/pl_infos.h
@@ -1,3 +1,7 @@
+/*
+ * Stores informations about all avaiable players for a game.  
+ */
+
 #ifndef __PLINFO_H__
 #define __PLINFO_H__
 
@@ -7,6 +11,12 @@
 #include "fakes/pl_info.h"
 #include "fakes/glibmm_decl.h"
 
+/*
+ * Using singleton pattern
+ * PLInfos class is a simple colletion of PlInfo objects. It supports accessing,
+ * adding, removing and pernament storing on a disk.  
+ */
+
 class PlInfos {
 private:
 
@@ -15,21 +25,39 @@ private:
     std::vector<PlInfo*> ais;
 
     static PlInfos instance;
-    
+
     PlInfos ();
-    
+
     ~PlInfos ();
 
+    /*
+     * Loads human players.
+     */
     void load_players ();
 
+    /*
+     * Loads all AI players or create default's ones if none was found.
+     */
     void load_ais ();
 
+    /*
+     * Saves human players.
+     */
     void save_players ();
 
+    /*
+     * Saves AI players
+     */
     void save_ais ();
-    
+
+    /*
+     * Loads one AI player
+     */
     void load_ai (AIInfo& info, const Glib::ustring& section);
-    
+
+    /*
+     * Stores one AI player. Different AIs are saveing different informations.
+     */
     void save_ai (const AIInfo& info, const Glib::ustring& section);
 
 public:
@@ -37,20 +65,31 @@ public:
 
     void save ();
 
+    /*
+     * Returns count of all players, humans and AIs together.
+     */
     size_t count () const;
 
     size_t players_count () const;
 
     size_t ais_count () const;
 
-    const PlInfo& operator[] (size_t index) const;
-
+    /*
+     * Accesing one player. Humans are indexed in an interval between 0 and
+     * players_count - 1, AIs between players_count and players_count + 
+     * ais_count - 1. 
+     */
     PlInfo& operator[] (size_t index);
+    const PlInfo& operator[] (size_t index) const;
 
     void add (const PlInfo& info);
 
+    /*
+     * Remove a player with specifed index from the colletion. It is using same
+     * indexing as accessing. 
+     */
     void remove (size_t idi);
-    
+
     static PlInfos& get_instance ();
 };
 
@@ -60,7 +99,7 @@ inline size_t PlInfos::count () const {
     return players.size () + ais.size ();
 }
 
-inline size_t PlInfos::players_count () const{
+inline size_t PlInfos::players_count () const {
     return players.size ();
 }
 
@@ -78,7 +117,7 @@ inline const PlInfo & PlInfos::operator [] (size_t index) const {
             - players.size ()];
 }
 
-inline PlInfos& PlInfos::get_instance() {
+inline PlInfos& PlInfos::get_instance () {
     return instance;
 }
 
