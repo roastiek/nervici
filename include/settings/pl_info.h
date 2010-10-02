c7fafa77c6fb5c519d81e388d331961556b14147
bobo <mucha.bobo@gmail.com>
2010-10-02 17:25:16 +0200

    Added documentation of pl_info.h

--- include/settings/pl_info.h
+++ include/settings/pl_info.h
@@ -1,8 +1,10 @@
 /* 
  * File:   pl_info.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 22. červenec 2010, 15:30
+ * 
+ * Stores information about players, both humans and AIs.
  */
 
 #ifndef PL_INFO_H
@@ -13,21 +15,50 @@
 #include "pl_type.h"
 #include "ais/ai_info.h"
 
+/*
+ * Stores keys which player uses to control its nerve.
+ */
 struct PlKeys {
     int left;
     int right;
     int jump;
 };
 
+/*
+ * All informations about a player that are needed to store.
+ */
 struct PlInfo {
+    /*
+     * nerve color
+     */
     uint32_t color;
+
+    /*
+     * whether represents human or AI
+     */
     PlTypes type;
+
+    /*
+     * player's name
+     */
     Glib::ustring name;
+
+    /*
+     * Specific informations for human or ai player.
+     */
     union {
         PlKeys keys;
         AIInfo ai;
     };
+
+    /*
+     * a sound profil of the player
+     */
     Glib::ustring profil;
+
+    /*
+     * pitch of player's voice
+     */
     int pitch;
 
     PlInfo ();
