3e3fa58667d2f2f0f8b7aa808b6468590d68048c
bobo <mucha.bobo@gmail.com>
2010-10-02 19:09:00 +0200

    Added documentation for team_info.h

--- include/settings/team_info.h
+++ include/settings/team_info.h
@@ -1,8 +1,10 @@
 /* 
  * File:   teaminfo.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 21. červenec 2010, 20:39
+ * 
+ * Stores information about a team.
  */
 
 #ifndef TEAMINFO_H
@@ -11,8 +13,20 @@
 #include <glibmm/ustring.h>
 
 struct TeamInfo {
+    /*
+     * Team's id. It helps with mapping between players and teams. It is not 
+     * saved and stay same.
+     */
     uint8_t id;
+    
+    /*
+     * Team's color.
+     */
     uint32_t color;
+    
+    /*
+     * Team's name.
+     */
     Glib::ustring name;
 };
 
