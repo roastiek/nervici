b59da214787d89726ca4b5e898b5519acc81c2de
bobo <mucha.bobo@gmail.com>
2010-10-03 00:21:55 +0200

    Added documentation for sound_profile.h

--- include/engine/sound_profile.h
+++ include/engine/sound_profile.h
@@ -2,7 +2,7 @@
  * sound_profile.h
  *
  *  Created on: 10.9.2010
- *      Author: bobo
+ *      Author: deedrah
  */
 
 #ifndef SOUND_PROFILE_H_
@@ -10,8 +10,17 @@
 
 #include <glibmm/ustring.h>
 
+/*
+ * Represents one sound profile.
+ */
 struct SoundProfile {
+    /*
+     * Sound profile's name, derived from directory name.
+     */
     Glib::ustring name;
+    /*
+     * Whole path to sound profile's directory.
+     */
     Glib::ustring directory;
 };
 
