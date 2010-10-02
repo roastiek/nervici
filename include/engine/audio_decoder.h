88769a2e16e4482f981d03f1a9b17ee3e5ea915d
bobo <mucha.bobo@gmail.com>
2010-10-02 23:12:11 +0200

    Added documentation for audio_decoder.h

--- include/engine/audio_decoder.h
+++ include/engine/audio_decoder.h
@@ -1,6 +1,6 @@
 /* 
  * File:   audio_decoder.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 15. srpen 2010, 16:59
  */
@@ -12,19 +12,37 @@
 
 #include "fakes/glibmm_decl.h"
 
+/*
+ * Interface for audio decoders. It is used for loading audio files. 
+ */
 class AudioDecoder {
 public:
     virtual ~AudioDecoder () {
     }
 
+    /*
+     * Opens a specifed file. Returns if it is a valid audio file.
+     */
     virtual bool open (const Glib::ustring& filename) = 0;
 
+    /*
+     * Return format of audio (16 bit, 8 bit).
+     */
     virtual ALenum get_format () const = 0;
 
+    /*
+     * Return samle frequency of audio.
+     */
     virtual ALsizei get_frequency () const = 0;
 
+    /*
+     * Reads data from file and decodes them into PCM format.
+     */
     virtual size_t read (char* buffer, size_t len) = 0;
 
+    /*
+     * Return length of audio file in secodns.
+     */
     virtual double get_length (const Glib::ustring& filename) = 0;
 };
 
