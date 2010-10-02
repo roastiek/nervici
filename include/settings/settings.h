820c90a68a2541683a17933c9f995794918159f2
bobo <mucha.bobo@gmail.com>
2010-10-02 18:14:07 +0200

    All types of setting files are now explicitly definded by enum SettingFiles. Added documentation for settings.h

--- include/settings/settings.h
+++ include/settings/settings.h
@@ -2,19 +2,33 @@
  * settings.h
  *
  *  Created on: 20.8.2010
- *      Author: bobo
+ *      Author: deedrah
+ *     
+ * Provides colletions of all Setting objects.    
  */
 
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
 
-#include <vector>
-
 #include "fakes/setting.h"
 
+/*
+ * All types of setting files, except of the last one which represents total 
+ * count of setting files.
+ */
+enum SettingFiles {
+    SF_app, SF_game, SF_players, SF_ais, SF_teams, SF_count
+};
+
+/*
+ * Using sigleton pattern
+ * Settings class group toghether all setting files of the appilication. Options
+ * are diveded into separeted files, because some of the options will not be 
+ * changed so often as the others.
+ */
 class Settings {
 private:
-    std::vector<Setting*> settings;
+    Setting* settings[SF_count];
 
     static Settings instance;
 
@@ -23,19 +37,43 @@ private:
     ~Settings ();
 
 public:
-
+    /*
+     * Loads all setting files.
+     */    
     void load ();
 
+    /*
+     * Saves all setting files.
+     */
     void save ();
 
+    /*
+     * Returns app setting object. It is used to store information about sound 
+     * and graphics.
+     */
     Setting& app ();
 
+    /*
+     * Returns players setting object. It is used to store information about 
+     * players.
+     */
     Setting& players ();
 
+    /*
+     * Returns ais setting object. It is used to store information about ais.
+     */
     Setting& ais ();
 
+    /*
+     * Returns game setting object. It is used to store information about the 
+     * game (choosen players, mod, mod options). 
+     */
     Setting& game ();
 
+    /*
+     * Returns teams setting object. It is used to store information about 
+     * teams.
+     */
     Setting& teams ();
 
     static Settings& get_instance ();
@@ -44,23 +82,23 @@ public:
 extern Settings& settings;
 
 inline Setting& Settings::app () {
-    return *settings[0];
+    return *settings[SF_app];
 }
 
 inline Setting& Settings::game () {
-    return *settings[1];
+    return *settings[SF_game];
 }
 
 inline Setting& Settings::players () {
-    return *settings[2];
+    return *settings[SF_players];
 }
 
 inline Setting& Settings::ais () {
-    return *settings[3];
+    return *settings[SF_ais];
 }
 
 inline Setting& Settings::teams () {
-    return *settings[4];
+    return *settings[SF_teams];
 }
 
 inline Settings& Settings::get_instance () {
--- src/settings/settings.cpp
+++ src/settings/settings.cpp
@@ -14,31 +14,31 @@ Settings Settings::instance;
 Settings& settings = Settings::get_instance ();
 
 Settings::Settings () {
-    settings.push_back (new Setting ("nervici.conf"));
-    settings.push_back (new Setting ("game.conf"));
-    settings.push_back (new Setting ("players.conf"));
-    settings.push_back (new Setting ("plastiks.conf"));
-    settings.push_back (new Setting ("teams.conf"));
+    settings[SF_app] = new Setting ("nervici.conf");
+    settings[SF_game] = new Setting ("game.conf");
+    settings[SF_players] = new Setting ("players.conf");
+    settings[SF_ais] = new Setting ("plastiks.conf");
+    settings[SF_teams] = new Setting ("teams.conf");
 }
 
 Settings::~Settings () {
     logger.fineln ("freeing settings");
-    for (size_t si = 0; si < settings.size (); si++) {
+    for (size_t si = 0; si < SF_count; si++) {
         delete settings[si];
+        settings[si] = NULL;
     }
-    settings.clear ();
 }
 
 void Settings::load () {
     logger.fineln ("loading settings");
-    for (size_t si = 0; si < settings.size (); si++) {
+    for (size_t si = 0; si < SF_count; si++) {
         settings[si]->load ();
     }
 }
 
 void Settings::save () {
     logger.fineln ("saving settings");
-    for (size_t si = 0; si < settings.size (); si++) {
+    for (size_t si = 0; si < SF_count; si++) {
         settings[si]->save ();
     }
 }
