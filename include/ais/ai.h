4bfaa5c839850be4f37ee7883b7edcdd73ceb506
bobo <mucha.bobo@gmail.com>
2010-10-02 19:38:15 +0200

    Added documentation for ai.h

--- include/ais/ai.h
+++ include/ais/ai.h
@@ -2,7 +2,9 @@
  * ai.h
  *
  *  Created on: 30.9.2010
- *      Author: bobo
+ *      Author: deedrah
+ *      
+ * Interface for AI algorithms.     
  */
 
 #ifndef AI_H_
@@ -12,16 +14,32 @@
 #include "game/key_state.h"
 #include "fakes/player.h"
 
+/*
+ * AI interface simulates a living player. Because in each round can a player do
+ * only one command, it simply calculates and returns the best one.
+ */
 class AI {
 public:
     virtual ~AI ();
 
+    /*
+     * Starts computing of the next best step in separeted thread.
+     */
     virtual void calc (int jumptime, plsize_tu head) = 0;
 
+    /*
+     * Force to finish calculation.
+     */
     virtual void finish () = 0;
-
+    
+    /*
+     * Waits for calculation end and returns its result.
+     */
     virtual KeySt get_next_step () = 0;
 
+    /*
+     * Creates specific AI for the player. For human player it returns NULL.
+     */
     static AI* create (Player& player);
 };
 
