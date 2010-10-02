f0b542777e2e4a16deea95cf895bd5e23e9e738b
bobo <mucha.bobo@gmail.com>
2010-10-03 00:16:22 +0200

    Added documentation for music_type.h

--- include/engine/music_type.h
+++ include/engine/music_type.h
@@ -1,6 +1,6 @@
 /* 
  * File:   music_type.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 22. červenec 2010, 15:51
  */
@@ -8,8 +8,30 @@
 #ifndef MUSIC_TYPE_H
 #define	MUSIC_TYPE_H
 
+/*
+ * Represent different music types.
+ */
 enum MusicType {
-    MT_GameShort, MT_GameLong, MT_Menu, MT_Stat, MT_Count
+    /*
+     * Music for a game shorter than 1 min 30 sec.
+     */
+    MT_GameShort,
+    /*
+     * Music for a game longer than 1 min 30 sec.
+     */
+    MT_GameLong,
+    /*
+     * Music for a menu.
+     */
+    MT_Menu,
+    /*
+     * Music for a statistic.
+     */
+    MT_Stat,
+    /*
+     * Count of music types.
+     */
+    MT_Count
 };
 
 #endif	/* MUSIC_TYPE_H */
