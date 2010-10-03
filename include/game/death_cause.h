f1472302f26fd287f40303dd298f820dc4f5df22
bobo <mucha.bobo@gmail.com>
2010-10-03 11:57:03 +0200

    Added documentation for death_cause.h

--- include/game/death_cause.h
+++ include/game/death_cause.h
@@ -1,8 +1,9 @@
 /* 
  * File:   death_cause.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 17. srpen 2010, 13:02
+ * Information about a death cause of player.
  */
 
 #ifndef DEATH_CAUSE_H
@@ -10,10 +11,37 @@
 
 #include "int_type.h"
 
+/*
+ * Types of all possible deaths.
+ */
 enum DeathCauses {
-    DC_none, DC_wall, DC_killed, DC_self, DC_smile
+    /*
+     * Player will not die.
+     */
+    DC_none,
+    /*
+     * Player has crashed into a wall.
+     */
+    DC_wall,
+    /*
+     * Player has been killed by another player.
+     */
+    DC_killed,
+    /*
+     * Player has commited suicide.
+     */
+    DC_self,
+    /*
+     * Player has crashed int a hard smile.
+     */
+    DC_smile
 };
 
+/*
+ * Contains type of the death and additiona informations for some causes.
+ * For DC_killed, murded option is set.
+ * For DC_smile, smile option is set.
+ */
 struct DeathCause {
 public:
     DeathCauses cause;
