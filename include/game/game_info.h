7fae8be07e2796a986270fc851c4d2af04914365
bobo <mucha.bobo@gmail.com>
2010-10-03 13:23:49 +0200

    Added documentation for game_info.h

--- include/game/game_info.h
+++ include/game/game_info.h
@@ -1,8 +1,10 @@
 /* 
  * File:   game_info.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 22. červenec 2010, 13:54
+ * 
+ * All information needed to start a game.
  */
 
 #ifndef GAME_INFO_H
@@ -17,20 +19,36 @@
 #include "fakes/team_info.h"
 
 struct GameInfo {
+    /*
+     * ID of mod, that shall be used.
+     */
     int mod_id;
     
+    /*
+     * Base game setting as a speed, rounds...
+     */
     GameSetting setting;
     
+    /*
+     * Counts of all smiles.
+     */
     SmileSetting smiles;
     
+    /*
+     * List of teams for the game. I tcan be empty.
+     */
     std::vector<const TeamInfo*> team_infos;
     
-    std::vector<const PlInfo*> pl_infos;
     /*
-     * maping between pl_infos and team_infos
-     * values from 0..teams_infos.size () inclusive,
-     * last value presents hidden "fake" default team,
-     * so every player belong to some team 
+     * List of players for the game.
+     */
+    std::vector<const PlInfo*> pl_infos;
+    
+    /*
+     * Maping between pl_infos and team_infos. Values are from 0 to 
+     * teams_infos.size () inclusive, last value reprecents hidden "fake" 
+     * default team, used for players without an explicit assigned team. Using
+     * same indexing as pl_infos. 
      */
     std::vector<plid_tu> pl_teams;
 };
