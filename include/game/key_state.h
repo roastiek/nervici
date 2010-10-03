140a49aeab042757519cde50a8d5f1416b890972
bobo <mucha.bobo@gmail.com>
2010-10-03 13:27:17 +0200

    Added documentation for key_state.h

--- include/game/key_state.h
+++ include/game/key_state.h
@@ -1,6 +1,6 @@
 /* 
  * File:   key_state.h
- * Author: bobo
+ * Author: deedrah
  *
  * Created on 22. červenec 2010, 13:57
  */
@@ -8,8 +8,31 @@
 #ifndef KEY_STATE_H
 #define	KEY_STATE_H
 
+/*
+ * State of pressed keys by player. Used for generelization input of human and 
+ * ai players.
+ */
 typedef enum KeySt {
-    KS_None, KS_Left, KS_Right, KS_Jump, KS_Power
+    /*
+     * No key pressed.
+     */
+    KS_None,
+    /*
+     * Only "left" key pressed. 
+     */
+    KS_Left,
+    /*
+     * Only "right" key pressed.
+     */
+    KS_Right,
+    /*
+     * "Jump" key preseed.
+     */
+    KS_Jump,
+    /*
+     * Both "left" and "right" keys pressed and same time. Not used now.
+     */
+    KS_Power
 } KeySt;
 
 #endif	/* KEY_STATE_H */
